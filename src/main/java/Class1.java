e22d11ef3b6e3c7d81b444639e3b6bc0ddf15c9c
AnastasiyaPC <anastasiya.vynogradska@naturalint.com>
2017-12-13 06:33:05 +0200

    test createListTutors is created

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,109 +1,53 @@
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
 import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Class1 {
-    private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-
-    @Before
-    public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        baseUrl = "http://skillsup.ua/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
+public class Class1 extends WebDriverSettings {
+    private By memberLocator  =  By.className("member");
+    private By tutorNameLocator  =  By.className("name");
+    private By tutorPositionLocator  =  By.className("position");
 
     @Test
-    public void testExample3() throws Exception {
+    public void createListTutors() throws Exception {
         driver.get(baseUrl + "/about/our-team/");
-        //driver.findElement(By.xpath("(//img[@alt='photo'])[1]")).click();
-//        driver.findElement(By.xpath("(//img[@alt='photo'])[2]")).click();
-//        driver.findElement(By.xpath("(//img[@alt='photo'])[3]")).click();
-        driver.findElement(By.partialLinkText ("Ольга Симчак")).click();
-        driver.navigate().back();
-        driver.findElement(By.partialLinkText ("Анастасия Педоренко")).click();
-        driver.navigate().back();
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
+        List<WebElement> listMemberWebElements = driver.findElements(memberLocator);
+        System.out.println(listMemberWebElements.size());
+        List<Tutor> listTutors = new ArrayList<Tutor>();
+        for (WebElement currentMember : listMemberWebElements) {
+            String tutorName = currentMember.findElement(tutorNameLocator).getText();
+            String tutorPosition = currentMember.findElement(tutorPositionLocator).getText();
+            Tutor  tutor = new Tutor(tutorName, tutorPosition);
+            System.out.println(tutor.toString());
+            listTutors.add(tutor);
         }
+        System.out.println("listTutors size is "+listTutors.size());
+    }
+    private String getPosition (String tutorName) {
+        String tutorPosition=null;
+        
+
+        return tutorPosition;
+
+    }
+    @Test
+        public String getPosition("Анастасия Педоренко") {
     }
 
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-
-//        FirefoxDriver wd;
-//
-//        @Before
-//        public void setUp() throws Exception {
-//            wd = new FirefoxDriver();
-//            wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+//    private boolean isElementPresent(By by) {
+//        try {
+//            driver.findElement(by);
+//            return true;
+//        } catch (NoSuchElementException e) {
+//            return false;
 //        }
-//
-//        @Test
-//        public void BaseTest() {
-//            wd.get("http://skillsup.ua/about/our-team/");
-//            wd.findElement(By.linkText("Ольга Симчак\n            \n            \n              тренер, PSPO, PSM")).click();
-//            wd.navigate().back();
-//            wd.findElement(By.linkText("Анастасия Педоренко\n            \n            \n              тренер, PSPO")).click();
-//            wd.findElement(By.xpath("//div[@class='text']//a[.='Погружение в Scrum']")).click();
-//            wd.navigate().back();
-//            wd.navigate().back();
-//            wd.findElement(By.linkText("Виктория Писаренко\n            \n            \n              тренер, PSM, ISTQB CTFL")).click();
-//            wd.findElement(By.xpath("//div[@class='text']//a[.='Погружение в Scrum']")).click();
-//            wd.navigate().back();
-//            wd.navigate().back();
+//    }
+//    private boolean isAlertPresent() {
+//        try {
+//            driver.switchTo().alert();
+//            return true;
+//        } catch (NoAlertPresentException e) {
+//            return false;
 //        }
-//
-//        @After
-//        public void tearDown() {
-//            wd.quit();
-//        }
-//
-//        public static boolean isAlertPresent(FirefoxDriver wd) {
-//            try {
-//                wd.switchTo().alert();
-//                return true;
-//            } catch (NoAlertPresentException e) {
-//                return false;
-//            }
-//        }
-
-
-
-
-
-
-
-
-
+//    }
 }
